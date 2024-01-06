25d3f6cb79889990bcf16076700b8c33ba6e2f30
NoeOn <alxg5516@gmail.com>
2024-01-05 23:21:03 -0600

    Agregue a la función main la conexión de mi base de datos y levante mi servidor http

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
-	"log"
+    "log"
+    "net/http"
+    "os"
 
-	"github.com/joho/godotenv"
+    "github.com/joho/godotenv"
+    "github.com/NoeAlejandroRodriguezMoto/API-GO/database"
 )
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(err)
-	}
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal(err)
+    }
 }
 
-func main() {}
+func main() {
+    db := database.ConnectDB()
+    defer db.Close()
+
+    port := os.Getenv("GO_PORT")
+    if port == "" {
+        log.Fatal("$GO_PORT must be set")
+    }
+
+    log.Printf("Listening on port %s\n", port)
+    log.Fatal(http.ListenAndServe(":"+port, nil))
+}
