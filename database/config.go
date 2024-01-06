893a8c388f4c00b530ea630aebe3ee12a32fa2ac
NoeOn <alxg5516@gmail.com>
2024-01-05 22:51:25 -0600

    Remplaze las constantes de la configuración de mi base de datos a agregar directamente en la función variables de entorno, agregue las variables de entorno en mi .env.example

--- database/config.go
+++ database/config.go
@@ -4,21 +4,19 @@ import (
     "database/sql"
     "fmt"
     "log"
+    "os"
 
     _ "github.com/lib/pq"
 )
 
-const (
-    host     = ""
-    port     = 
-    user     = ""
-    password = ""
-    dbname   = ""
-)
-
 func ConnectDB() *sql.DB {
-    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-        "password=%s dbname=%s sslmode=disable",
+    host := os.Getenv("DB_HOST")
+    port := os.Getenv("DB_PORT")
+    user := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASS")
+    dbname := os.Getenv("DB_NAME")
+
+    psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
         host, port, user, password, dbname)
 
     db, err := sql.Open("postgres", psqlInfo)
