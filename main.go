e883144f2cfd3ee81098f4cbe2205be445920d5c
NoeOn <alxg5516@gmail.com>
2024-01-07 18:38:11 -0600

    Reincie el proyecto para crear codigo mas legible y seguir mejores practicas con la concurrencia

--- main.go
+++ main.go
@@ -1,37 +1,21 @@
-package main
+package main 
 
 import (
-    "log"
-    "os"
-
-    "github.com/gin-gonic/gin"
-    "github.com/joho/godotenv"
-    "github.com/NoeAlejandroRodriguezMoto/API-GO/database"
-    "github.com/NoeAlejandroRodriguezMoto/API-GO/routes"
-    "github.com/NoeAlejandroRodriguezMoto/API-GO/controllers"
+	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
-func init() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal(err)
-    }
+type PingController struct {}
+
+func (PingController) Ping(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
 }
 
 func main() {
-    db := database.ConnectDB()
-    defer db.Close()
-
-    clientController := controllers.ClientController{DB: db}
-
-    router := gin.Default()
-    routes.ClientRoutes(router, &clientController)
-
-    port := os.Getenv("GO_PORT")
-    if port == "" {
-        log.Fatal("$GO_PORT must be set")
-    }
-
-    log.Printf("Listening on port %s\n", port)
-    log.Fatal(router.Run(":" + port))
+	r := gin.Default()
+	r.GET("/ping", PingController{}.Ping)
+	r.Run()
 }
+
