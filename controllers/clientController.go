e4c1f2143cb585c7f20169385c34c4db664815a7
NoeOn <alxg5516@gmail.com>
2024-01-07 23:58:41 -0600

    Cree la función de GetClients para obtener los registros de mi table clients como respuesta en json

--- controllers/clientController.go
+++ controllers/clientController.go
@@ -22,3 +22,29 @@ func CreateClient(c *gin.Context, db *sql.DB) {
 
     c.JSON(http.StatusOK, gin.H{"data": client})
 }
+
+func GetClients(c *gin.Context, db *sql.DB) {
+    rows, err := db.Query("SELECT firstname, lastname, birthdate, phone, email, password FROM clients")
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+    defer rows.Close()
+
+    var clients []models.Client
+    for rows.Next() {
+        var client models.Client
+        if err := rows.Scan(&client.FirstName, &client.LastName, &client.BirthDate, &client.Phone, &client.Email, &client.Password); err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+            return
+        }
+        clients = append(clients, client)
+    }
+
+    if err := rows.Err(); err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{"data": clients})
+}
