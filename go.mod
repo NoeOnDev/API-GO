eda5097bacee651fdc2b62ef3be9b4855dddc582
NoeOn <alxg5516@gmail.com>
2024-01-07 21:29:10 -0600

    Agregue variables de entorno al proyecto con godotenv y crearé la API con el framework gin

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/NoeAlejandroRodriguezMoto/API-GO
 
 go 1.21.5
 
-require github.com/gin-gonic/gin v1.9.1
+require (
+	github.com/gin-gonic/gin v1.9.1
+	github.com/joho/godotenv v1.5.1
+)
 
 require (
 	github.com/bytedance/sonic v1.10.2 // indirect
--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main 
 
 import (
-	"net/http"
+	"os"
+	"log"
+	"github.com/joho/godotenv"
 	"github.com/gin-gonic/gin"
 )
 
-type PingController struct {}
-
-func (PingController) Ping(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	})
-}
-
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	PORT := os.Getenv("GO_PORT")
+
 	r := gin.Default()
-	r.GET("/ping", PingController{}.Ping)
-	r.Run()
+	r.Run(":" + PORT)
 }
 
