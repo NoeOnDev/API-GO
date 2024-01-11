f14ff6e7922ee95ff0431eda18a9b9738e18d4b2
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-01-11 00:28:56 -0600

    He agregado validaciones en mi estructura User para no aceptar celdas vacias

--- models/userModel.go
+++ models/userModel.go
@@ -1,12 +1,21 @@
 package models
 
+import (
+    "github.com/go-playground/validator/v10"
+)
+
 type User struct {
     TableName struct{} `pg:"users"`
     ID        int      `pg:"id,pk"`
-    FirstName string   `pg:"first_name"`
-    LastName  string   `pg:"last_name"`
-    Username  string   `pg:"username,unique"`
-    Phone     string   `pg:"phone"`
-    Email     string   `pg:"email,unique"`
-    Password  string   `pg:"password"`
+    FirstName string   `pg:"first_name" validate:"required"`
+    LastName  string   `pg:"last_name" validate:"required"`
+    Username  string   `pg:"username,unique" validate:"required"`
+    Phone     string   `pg:"phone" validate:"required"`
+    Email     string   `pg:"email,unique" validate:"required,email"`
+    Password  string   `pg:"password" validate:"required"`
+}
+
+func (u *User) Validate() error {
+    validate := validator.New()
+    return validate.Struct(u)
 }
