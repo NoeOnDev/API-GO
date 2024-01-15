40c254f6ac8662c9eb6dba885563e8f43f66ed4c
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-01-14 22:26:40 -0600

    Agregue los bloques de las futuras funciones

--- main.go
+++ main.go
@@ -1,62 +1,28 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/joho/godotenv"
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
-    "os"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-type User struct {
-    gorm.Model
-    Name  string
-    Email string `gorm:"type:varchar(100);unique_index"`
-}
-
-var db *gorm.DB
-
 func main() {
-    err := godotenv.Load()
-    if err != nil {
-        panic("Error loading .env file")
-    }
-
-    dsn := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASS") + " dbname=" + os.Getenv("DB_NAME") + " port=" + os.Getenv("DB_PORT") + " sslmode=disable"
-    db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
-    if err != nil {
-        panic("failed to connect database")
-    }
-
-    db.AutoMigrate(&User{})
-
-    r := gin.Default()
-
-    r.GET("/users", GetUsers)
-    r.POST("/users", CreateUser)
-
-    r.Run()
+	envLoad()
+	databaseLoad()
+	serverAplication()
 }
 
-func GetUsers(c *gin.Context) {
-	var users []User
-	if err := db.Find(&users).Error; err != nil {
-		c.JSON(500, gin.H{"error": "Error retrieving users"})
-		return
+func envLoad() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
 	}
-	c.JSON(200, users)
 }
 
-func CreateUser(c *gin.Context) {
-	var user User
-	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(500, gin.H{"error": "Bad request"})
-		return
-	}
-	if err := db.Create(&user).Error; err != nil {
-		c.JSON(500, gin.H{"error": "Error creating user"})
-		return
-	}
-	c.JSON(200, user)
+func databaseLoad() {
+
+}
+
+func serverAplication() {
+	r := gin.Default()
+	r.Run()
 }
