88e997d0ac4ba049546abeb65608ba8ac4d28b26
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-01-15 11:06:02 -0600

    Agregué las funciones Save, BeforeSave, ValidatePassword y FindUserByUsernameOrEmail en mi modelo user

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21.5
 require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.18.0
 	gorm.io/driver/postgres v1.5.4
 	gorm.io/gorm v1.25.5
 )
@@ -37,7 +38,6 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.7.0 // indirect
-	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
--- models/user.go
+++ models/user.go
@@ -1,14 +1,49 @@
-
 package models
 
 import (
 	"github.com/NoeAlejandroRodriguezMoto/API-GO/database"
+
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
+	"html"
+	"strings"
 )
 
 type User struct {
 	gorm.Model
-	Usename string `json:"username"`
-	Email string `json:"email"`
-	Password string `json:"password"`
+	Username string `gorm:"size:255;not null;unique" json:"username"`
+	Email string `gorm:"size:100;not null;unique" json:"email"`
+	Password string `gorm:"size:255;not null;" json:"password"`
+}
+
+func (user *User) Save() (*User, error) {
+	err := database.Database.Create(&user).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return user, nil
+}
+
+func (user *User) BeforeSave(*gorm.DB) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hashedPassword)
+	user.Username = html.EscapeString(strings.TrimSpace(user.Username))
+	user.Email = html.EscapeString(strings.TrimSpace(user.Email))
+	return nil
+}
+
+func (user *User) ValidatePassword(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+}
+
+func FindUserByUsernameOrEmail(usernameOrEmail string) (User, error) {
+    var user User
+    err := database.Database.Where("username = ? OR email = ?", usernameOrEmail, usernameOrEmail).First(&user).Error
+    if err != nil {
+        return User{}, err
+    }
+    return user, nil
 }
