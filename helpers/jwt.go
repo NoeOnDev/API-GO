559c7010b2949c01e393938531788f829fa7c18e
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-03 15:12:04 -0600

    Agregadas dependencias de jwt/v4 en go.mod y go.sum

---
+++ helpers/jwt.go
@@ -0,0 +1,81 @@
+package helper
+
+import (
+	"github.com/NoeAlejandroRodriguezMoto/API-GO/models"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+var privateKey = []byte(os.Getenv("JWT_PRIVATE_KEY"))
+
+func GenerateJWT(user models.User) (string, error) {
+	tokenTTL, _ := strconv.Atoi(os.Getenv("TOKEN_TTL"))
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":  user.ID,
+		"iat": time.Now().Unix(),
+		"eat": time.Now().Add(time.Second * time.Duration(tokenTTL)).Unix(),
+	})
+	return token.SignedString(privateKey)
+}
+
+func ValidateJWT(context *gin.Context) error {
+	token, err := getToken(context)
+
+	if err != nil {
+		return err
+	}
+
+	_, ok := token.Claims.(jwt.MapClaims)
+
+	if ok && token.Valid {
+		return nil
+	}
+
+	return errors.New("invalid token provided")
+}
+
+
+func CurrentUser(context *gin.Context) (models.User, error) {
+	err := ValidateJWT(context)
+	if err != nil {
+		return models.User{}, err
+	}
+
+	token, _ := getToken(context)
+	claims, _ := token.Claims.(jwt.MapClaims)
+	userId := uint(claims["id"].(float64))
+
+	user, err := models.FindUserWithPetsById(userId)
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
+}
+
+func getToken(context *gin.Context) (*jwt.Token, error) {
+	tokenString := getTokenFromRequest(context)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return privateKey, nil
+	})
+	return token, err
+}
+
+func getTokenFromRequest(context *gin.Context) string {
+	bearerToken := context.Request.Header.Get("Authorization")
+	splitToken := strings.Split(bearerToken, " ")
+	if len(splitToken) == 2 {
+		return splitToken[1]
+	}
+	return ""
+}
