8a42f27b2557f52aa2574d46d085fd89819b0e65
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-03 17:44:53 -0600

    Actualización de la función de búsqueda para incluir filtros de fecha y categoría.

---
+++ middlewares/jwtAuth.go
@@ -0,0 +1,22 @@
+package middlewares
+
+import (
+	"github.com/NoeAlejandroRodriguezMoto/API-GO/helpers"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func JWTAuthMiddleware() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		err := helpers.ValidateJWT(context)
+		if err != nil {
+			context.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication required"})
+			fmt.Println(err)
+			context.Abort()
+			return
+		}
+		context.Next()
+	}
+}
