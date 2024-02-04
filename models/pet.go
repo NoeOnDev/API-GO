6b9e9ee68da474e6c83320eba2f6eced19c80595
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-03 18:05:26 -0600

    Eliminar función GetAllPetsByUserID() en models/pet.go

--- models/pet.go
+++ models/pet.go
@@ -21,12 +21,3 @@ func (pet *Pet) SavePet() (*Pet, error) {
 	}
 	return pet, nil
 }
-
-func GetAllPetsByUserID(userID uint) ([]Pet, error) {
-    var pets []Pet
-    err := database.Database.Preload("User").Where("user_id = ?", userID).Find(&pets).Error
-    if err != nil {
-        return nil, err
-    }
-    return pets, nil
-}
